9b1649c69df53981f35d952cac2c99331cbdda86
eroauditore <alfa.fenrir@gmail.com>
2022-03-04 15:04:34 -0600

    initial sagas redux definition

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux';
+
+// import quiz from './quiz';
+
+const allReducers = combineReducers({});
+
+export default allReducers;
---
+++ src/redux/sagas/index.js
@@ -0,0 +1,8 @@
+import { all } from 'redux-saga/effects';
+
+// import quiz from './quiz';
+
+// Es donde se van a ejecutar todos los watchers
+export default function* rootSaga() {
+  yield all([]);
+}
